b47e7472873b6b43e73aac801a9bb1249cb51ce6
Ciara <ciarabrown1596@gmail.com>
2020-04-27 19:04:11 +0100

    fuel

--- app/src/main/java/com/example/carbonfootprinttrackerfinal/DomesticActivity.java
+++ app/src/main/java/com/example/carbonfootprinttrackerfinal/DomesticActivity.java
@@ -2,25 +2,47 @@ package com.example.carbonfootprinttrackerfinal;
 
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DomesticActivity extends AppCompatActivity {
 
     EditText totalDomestic, light, heat, water, waste;
     Button submit;
-    private DatabaseReference reff;
+
+    private String saveCurrentTime;
+    private String saveCurrentDate;
+
+    private String Light;
+    private String Heat;
+    private String Water;
+    private String Waste;
+    private String TotalDomestic;
+
+    private DatabaseReference DomesticReff;
     private FirebaseAuth myFirebaseAuth;
     private FirebaseDatabase myFirebaseDatabase;
+    private ProgressDialog loadingBar;
+    private String DomesticRandomKey;
     Domestic domestic;
 
     @Override
@@ -29,7 +51,7 @@ public class DomesticActivity extends AppCompatActivity {
         setContentView(R.layout.activity_domestic);
 
         myFirebaseAuth = FirebaseAuth.getInstance();
-        reff = FirebaseDatabase.getInstance().getReference("Domestic");
+        DomesticReff = FirebaseDatabase.getInstance().getReference("Domestic");
 
         totalDomestic = (EditText)findViewById(R.id.Domestic);
         light = (EditText)findViewById(R.id.Light);
@@ -37,32 +59,109 @@ public class DomesticActivity extends AppCompatActivity {
         water =(EditText)findViewById(R.id.Water);
         waste = (EditText)findViewById(R.id.Waste);
         submit = (Button) findViewById(R.id.button2);
+
+        loadingBar = new ProgressDialog(this);
+
         domestic = new Domestic();
-        reff = FirebaseDatabase.getInstance().getReference().child("Domestic");
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveDomesticInformation();
+                ValidateDomesticData();
             }
         });
     }
 
-    private void saveDomesticInformation(){
-        int TotalDomestic = Integer.parseInt((totalDomestic.getText().toString().trim()));
-        int Light = Integer.parseInt(light.getText().toString().trim());
-        int Heat = Integer.parseInt(heat.getText().toString().trim());
-        int Water = Integer.parseInt(water.getText().toString().trim());
-        int Waste = Integer.parseInt(waste.getText().toString().trim());
 
-        String id = reff.push().getKey();
+    private void ValidateDomesticData()
+    {
+        Heat = heat.getText().toString().trim();
+        Light = light.getText().toString().trim();
+        Water = water.getText().toString().trim();
+        Waste = waste.getText().toString().trim();
+        TotalDomestic = totalDomestic.getText().toString().trim();
 
-        Domestic domestic = new Domestic(id, TotalDomestic,Light,Heat,Water,Waste);
 
-        reff.child(id).setValue(domestic);
+        if (TextUtils.isEmpty(Heat))
+        {
+            Toast.makeText(this, "Please write heat ...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Light))
+        {
+            Toast.makeText(this, "Please enter light...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Water))
+        {
+            Toast.makeText(this, "Please enter water...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Waste))
+        {
+            Toast.makeText(this, "Please enter Waste...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            StoreDomesticInformation();
+        }
+    }
 
-        Toast.makeText(this, "Domestic Information saved", Toast.LENGTH_LONG).show();
 
+
+    private void StoreDomesticInformation()
+    {
+        loadingBar.setTitle("Add New Domestic data");
+        loadingBar.setMessage("Dear User, please wait while we are adding the domestic data.");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        DomesticRandomKey = saveCurrentDate + saveCurrentTime;
+
+        SaveDomesticInfoToDatabase();
+    }
+
+
+
+    private void SaveDomesticInfoToDatabase()
+    {
+        HashMap<String, Object> domMap = new HashMap<>();
+        domMap.put("Water",Water);
+        domMap.put("Waste", Waste);
+        domMap.put("date", saveCurrentDate);
+        domMap.put("time", saveCurrentTime);
+        domMap.put("Heat", Heat);
+        domMap.put("Light", Light);
+        domMap.put("Total Domestic", TotalDomestic);
+
+        DomesticReff.child(DomesticRandomKey).updateChildren(domMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Intent intent = new Intent(DomesticActivity.this, HomeActivity.class);
+                            startActivity(intent);
+
+                            loadingBar.dismiss();
+                            Toast.makeText(DomesticActivity.this, "Domestic Data is added successfully..", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            String message = task.getException().toString();
+                            Toast.makeText(DomesticActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
 
--- app/src/main/java/com/example/carbonfootprinttrackerfinal/FuelActivity.java
+++ app/src/main/java/com/example/carbonfootprinttrackerfinal/FuelActivity.java
@@ -2,25 +2,58 @@ package com.example.carbonfootprinttrackerfinal;
 
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class FuelActivity extends AppCompatActivity {
 
-        EditText reg, date, amount, type;
+        EditText reg, date, amount, type,price;
         Button Submit;
-        private DatabaseReference reff;
+
+        private String FuelType;
+        private String FuelDate;
+        private String FuelAmout;
+        private String FuelPrice;
+        private String VehicleUsed;
+
+        private String saveCurrentTime;
+        private String saveCurrentDate;
+        private String VehicleReg;
+
+        private Spinner vehicleSpinner;
+        private DatabaseReference FuelReff, databaseReff;
         private FirebaseAuth myFirebaseAuth;
         private FirebaseDatabase myFirebaseDatabase;
+        ValueEventListener listener;
+        ArrayAdapter<String> adapter;
+        ArrayList<String> spinnerDataList;
+        private ProgressDialog loadingBar;
+        private String userRandomKey;
         Fuel fuel;
 
         @Override
@@ -29,37 +62,141 @@ public class FuelActivity extends AppCompatActivity {
                 setContentView(R.layout.activity_fuel);
 
                 myFirebaseAuth = FirebaseAuth.getInstance();
-                reff = FirebaseDatabase.getInstance().getReference("Fuel");
+                FuelReff = FirebaseDatabase.getInstance().getReference("Fuel");
 
-                reg = (EditText) findViewById(R.id.vehicleReg);
+                databaseReff = FirebaseDatabase.getInstance().getReference("Vehicles");
+
+                vehicleSpinner = findViewById(R.id.veh_Spinner);
+
                 amount = (EditText) findViewById(R.id.fuelAmount);
                 type = (EditText) findViewById(R.id.fuelType);
                 date = (EditText) findViewById(R.id.fuelDate);
+                price = findViewById(R.id.fuelPrice);
+
                 Submit = (Button) findViewById(R.id.button4);
+                loadingBar = new ProgressDialog(this);
 
 
                 Submit.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                                saveFuelInformation();
+                                ValidateFuelData();
                         }
                 });
+
+                spinnerDataList = new ArrayList<>();
+                adapter = new ArrayAdapter<String>(FuelActivity.this,
+                        android.R.layout.simple_spinner_dropdown_item,spinnerDataList);
+
+                vehicleSpinner.setAdapter(adapter);
+                retrieveData();
+
         }
 
-        public void saveFuelInformation() {
-                Double Amount = Double.parseDouble(amount.getText().toString().trim());
-                String Reg = reg.getText().toString().trim();
-                String Type = type.getText().toString().trim();
-                String Date = date.getText().toString().trim();
+        private void ValidateFuelData()
+        {
+                FuelType = type.getText().toString().trim();
+                FuelAmout = amount.getText().toString().trim();
+                FuelDate = date.getText().toString().trim();
+                FuelPrice = price.getText().toString().trim();
 
-                String id = reff.push().getKey();
+                if (TextUtils.isEmpty(FuelType))
+                {
+                        Toast.makeText(this, "Please write the Fuel type ...", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(FuelAmout))
+                {
+                        Toast.makeText(this, "Please enter the fuel amount...", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(FuelDate))
+                {
+                        Toast.makeText(this, "Please enter date the fuel was purchased...", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(FuelPrice))
+                {
+                        Toast.makeText(this, "Please enter the price of the fuel...", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                        StoreFuelInformation();
+                }
+        }
 
-                Fuel fuel = new Fuel(id,Reg,Amount,Type,Date);
+        private void StoreFuelInformation()
+        {
+                loadingBar.setTitle("Add New Fuel");
+                loadingBar.setMessage("Dear User, please wait while we are adding the new fuel info.");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
 
-                reff.child(id).setValue(fuel);
+                Calendar calendar = Calendar.getInstance();
 
-                Toast.makeText(this, "Fuel Information saved", Toast.LENGTH_LONG).show();
+                SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+                saveCurrentDate = currentDate.format(calendar.getTime());
 
+                SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+                saveCurrentTime = currentTime.format(calendar.getTime());
+
+                userRandomKey = saveCurrentDate + saveCurrentTime;
+
+                SaveFuelInfoToDatabase();
+        }
+
+
+
+        private void SaveFuelInfoToDatabase()
+        {
+                HashMap<String, Object> fuelMap = new HashMap<>();
+                fuelMap.put("Fuel Type",FuelType);
+                fuelMap.put("Fuel Amount", FuelAmout);
+                fuelMap.put("date", saveCurrentDate);
+                fuelMap.put("time", saveCurrentTime);
+                fuelMap.put("Date Purchased", FuelDate);
+                fuelMap.put("Fuel Price", FuelPrice);
+
+                FuelReff.child(userRandomKey).updateChildren(fuelMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                        if (task.isSuccessful())
+                                        {
+                                                Intent intent = new Intent(FuelActivity.this, HomeActivity.class);
+                                                startActivity(intent);
+
+                                                loadingBar.dismiss();
+                                                Toast.makeText(FuelActivity.this, "Fuel data is added successfully..", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else
+                                        {
+                                                loadingBar.dismiss();
+                                                String message = task.getException().toString();
+                                                Toast.makeText(FuelActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                        }
+                                }
+                        });
+        }
+
+        public void retrieveData()
+        {
+                listener = databaseReff.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                        {
+                                for(DataSnapshot item:dataSnapshot.getChildren())
+                                {
+                                        VehicleReg = item.child("Vehicle Reg").getValue(String.class);
+                                        spinnerDataList.add(VehicleReg);
+                                }
+                                adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                });
 
         }
 

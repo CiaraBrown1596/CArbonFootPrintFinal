c33f658476c92bdd378f9a82f5cc35dc309a3358
Ciara <ciarabrown1596@gmail.com>
2020-05-05 18:39:22 +0100

    view vehicles.

---
+++ app/src/main/java/com/example/carbonfootprinttrackerfinal/ViewFlights.java
@@ -0,0 +1,14 @@
+package com.example.carbonfootprinttrackerfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewFlights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_flights);
+    }
+}
